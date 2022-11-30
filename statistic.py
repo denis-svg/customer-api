a0a1e9cf96e7dc77a26dd2eb1feefa2ed8b54463
denis <dprodan289@gmail.com>
2022-11-30 17:39:15 +0200

    statisticsTime

--- statistic.py
+++ statistic.py
@@ -6,24 +6,26 @@ from statistics import mean
 
 statistic = Blueprint('statistics', __name__)
 
-@statistic.route('/api/statistics/<click>/device/<timestamp>')
+@statistic.route('/api/statistics/clicks/<event>/device/<timestamp>')
 @cache.cached(timeout=1000, query_string=True)
-def statisticsClicksDeviceView(click: str, timestamp: str):
+def statisticsClicksDeviceView(event: str, timestamp: str):
     if request.method == 'GET':
-        available_clicks = ["clicksToConvert", "clicksToShare"]
-        if click.lower() not in list(map(lambda c : c.lower(), available_clicks)):
+        available_events = ["Convert", "Share"]
+        event = event.lower()
+        if event not in list(map(lambda c : c.lower(), available_events)):
             return "Invalid click", 400
 
         available_timestamps = ["lastday", "thisweek", "thismonth"]
-        if timestamp.lower() not in available_timestamps:
+        timestamp = timestamp.lower()
+        if timestamp not in available_timestamps:
             return "Invalid timestamp", 400
 
-        clicks = {
-            "clickstoconvert" : "conversion",
-             "clickstoshare" : "share_experience",
+        events = {
+            "convert" : "conversion",
+             "share" : "share_experience",
         }
 
-        e_name = clicks[click]
+        e_name = events[event]
 
         timestamps = {
             "lastday": 0,
@@ -82,24 +84,25 @@ def statisticsClicksDeviceView(click: str, timestamp: str):
 
         return jsonify(out)
 
-@statistic.route('/api/statistics/<click>/locale/<timestamp>')
+@statistic.route('/api/statistics/clicks/<event>/locale/<timestamp>')
 @cache.cached(timeout=1000, query_string=True)
-def statisticsClicksLocaleView(click: str, timestamp: str):
+def statisticsClicksLocaleView(event: str, timestamp: str):
     if request.method == 'GET':
-        available_clicks = ["clicksToConvert", "clicksToShare"]
-        if click.lower() not in list(map(lambda c : c.lower(), available_clicks)):
+        available_events = ["Convert", "Share"]
+        event = event.lower()
+        if event not in list(map(lambda c : c.lower(), available_events)):
             return "Invalid click", 400
 
         available_timestamps = ["lastday", "thisweek", "thismonth"]
         if timestamp.lower() not in available_timestamps:
             return "Invalid timestamp", 400
 
-        clicks = {
-            "clickstoconvert" : "conversion",
-             "clickstoshare" : "share_experience",
+        events = {
+            "convert" : "conversion",
+             "share" : "share_experience",
         }
 
-        e_name = clicks[click]
+        e_name = events[event]
 
         timestamps = {
             "lastday": 0,
@@ -154,6 +157,175 @@ def statisticsClicksLocaleView(click: str, timestamp: str):
                         values.append(counter)
                         break
                     counter += 1
+            
+            if len(values) > 0:
+                out[locale[0]] = {"notFiltered":{"values":values, "mean":mean(values)}, 
+                                "filtered":{"values":flt.filter3(values), "mean":mean(flt.filter3(values))}}
+
+        cnxn.close()
+
+        return jsonify(out)
+
+@statistic.route('/api/statistics/time/<event>/device/<timestamp>')
+@cache.cached(timeout=1000, query_string=True)
+def statisticsTimeDeviceView(event: str, timestamp: str):
+    if request.method == 'GET':
+        available_events = ["Convert", "Share"]
+        event = event.lower()
+        if event not in list(map(lambda c : c.lower(), available_events)):
+            return "Invalid click", 400
+
+        available_timestamps = ["lastday", "thisweek", "thismonth"]
+        timestamp = timestamp.lower()
+        if timestamp not in available_timestamps:
+            return "Invalid timestamp", 400
+
+        events = {
+            "convert" : "conversion",
+             "share" : "share_experience",
+        }
+
+        e_name = events[event]
+
+        timestamps = {
+            "lastday": 0,
+            "thisweek": 6,
+            "thismonth": 29,
+        }
+        
+        day = timestamps[timestamp]
+
+        cnxn = getConnection()
+        cursor = cnxn.cursor()
+        
+        devices = ["Desktop", "Mobile"]
+
+        out = {} 
+
+        for device in devices:
+            events = cursor.execute(f"""DECLARE @day AS INT
+										SET @day = (SELECT DATEPART(dy, clicked_date)
+													FROM Events
+													WHERE clicked_date = (SELECT MAX(clicked_date)
+													FROM Events))
+										SELECT Events.person_id,
+												Events.event_name,
+												Events.clicked_date
+										FROM Events
+										INNER JOIN Persons
+										ON Persons.person_id = Events.person_id
+										WHERE DATEPART(dy, clicked_date) >= @day - ? AND device=?
+										ORDER BY clicked_date ASC""", day, device).fetchall()
+            
+            person_dict = {}
+
+            for event in events:
+                event_name = event[1]
+                person_id = event[0]
+                date = event[2]
+
+                if person_id not in person_dict:
+                    person_dict[person_id] = [[event_name, date]]
+                else:
+                    person_dict[person_id].append([event_name, date])
+            
+            values = []
+            
+            for person_id in person_dict.keys():
+                events = person_dict[person_id]
+                start_date = events[0][1]
+                for event in events:
+                    event_name = event[0]
+                    cur_date = event[1]
+                    if event_name == e_name:
+                        values.append(round((cur_date - start_date).total_seconds()))
+                        break
+            
+            out[device] = {"notFiltered":{"values":values, "mean":mean(values)}, 
+                            "filtered":{"values":flt.filter3(values), "mean":mean(flt.filter3(values))}}
+
+        cnxn.close()
+
+        return jsonify(out)
+
+@statistic.route('/api/statistics/time/<event>/locale/<timestamp>')
+@cache.cached(timeout=1000, query_string=True)
+def statisticsTimeLocaleView(event: str, timestamp: str):
+    if request.method == 'GET':
+        available_events = ["Convert", "Share"]
+        event = event.lower()
+        if event not in list(map(lambda c : c.lower(), available_events)):
+            return "Invalid click", 400
+
+        available_timestamps = ["lastday", "thisweek", "thismonth"]
+        timestamp = timestamp.lower()
+        if timestamp not in available_timestamps:
+            return "Invalid timestamp", 400
+
+        events = {
+            "convert" : "conversion",
+             "share" : "share_experience",
+        }
+
+        e_name = events[event]
+
+        timestamps = {
+            "lastday": 0,
+            "thisweek": 6,
+            "thismonth": 29,
+        }
+        
+        day = timestamps[timestamp]
+
+        cnxn = getConnection()
+        cursor = cnxn.cursor()
+        
+        locales = cursor.execute(f"""SELECT 	TOP(?)
+												locale
+										FROM Persons
+										GROUP BY locale
+										ORDER BY COUNT(locale) DESC""", 6).fetchall()
+
+        out = {} 
+
+        for locale in locales:
+            events = cursor.execute(f"""DECLARE @day AS INT
+										SET @day = (SELECT DATEPART(dy, clicked_date)
+													FROM Events
+													WHERE clicked_date = (SELECT MAX(clicked_date)
+													FROM Events))
+										SELECT Events.person_id,
+												Events.event_name,
+												Events.clicked_date
+										FROM Events
+										INNER JOIN Persons
+										ON Persons.person_id = Events.person_id
+										WHERE DATEPART(dy, clicked_date) >= @day - ? AND locale=?
+										ORDER BY clicked_date ASC""", day, locale[0]).fetchall()
+            
+            person_dict = {}
+
+            for event in events:
+                event_name = event[1]
+                person_id = event[0]
+                date = event[2]
+
+                if person_id not in person_dict:
+                    person_dict[person_id] = [[event_name, date]]
+                else:
+                    person_dict[person_id].append([event_name, date])
+            
+            values = []
+            
+            for person_id in person_dict.keys():
+                events = person_dict[person_id]
+                start_date = events[0][1]
+                for event in events:
+                    event_name = event[0]
+                    cur_date = event[1]
+                    if event_name == e_name:
+                        values.append(round((cur_date - start_date).total_seconds()))
+                        break
             
             if len(values) > 0:
                 out[locale[0]] = {"notFiltered":{"values":values, "mean":mean(values)}, 
