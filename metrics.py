b876a2a3fe45046d2d023aa7699cdccd1e4d87a4
denis <dprodan289@gmail.com>
2022-12-04 13:53:38 +0200

    new endpoint

--- metrics.py
+++ metrics.py
@@ -37,3 +37,37 @@ def getTotalClicks():
                         """).fetchall()
 
     return jsonify(list(map(lambda x: {"period": x[1], "value": x[0]}, res)))
+
+@metrics.route("/api/metrics/average/<metric>", methods=["GET"])
+@cache.memoize(timeout=1000)
+def getAverageMetric(metric: str):
+    valid_metrics = ["clicksToConvert",
+                     "clicksToShare", "timeToConvert", "timeToShare"]
+    if metric.lower() not in map(str.lower, valid_metrics):
+        return "Invalid metric", 400
+
+    timeframe = request.args.get("timeframe").lower(
+    ) if request.args.get("timeframe") is not None else "day"
+    timeframe = timeframe if timeframe in ["day", "week", "month"] else "day"
+    days = 1 if timeframe == "day" else 7 if timeframe == "week" else 30
+    grouping = "format(clicked_date, 'hh tt')" if timeframe == "day"\
+        else "format(clicked_date, 'yyyy-MM-dd')" if timeframe == "week"\
+        else "format(clicked_date, 'yyyy-MM-dd')"
+
+    cnxn = getConnection()
+    cursor = cnxn.cursor()
+
+    res = cursor.execute(f"""
+                        declare @latest datetime = (select max(clicked_date) from Events)
+                        select avg(cast({metric} as float)),
+                            {grouping}
+                        from persons_metric
+                        left join Events
+                        on persons_metric.person_id = Events.person_id
+                        where {metric} is not null
+                        and clicked_date > dateadd(day, {-days}, @latest)
+                        group by {grouping}
+                        order by parse({grouping} as datetime) asc
+                        """).fetchall()
+
+    return jsonify(list(map(lambda x: {"period": x[1], "value": x[0]}, res)))
