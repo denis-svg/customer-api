4a3d9aff6160e58ba17d25c7bc47bbeb4ad15b1b
denis <dprodan289@gmail.com>
2022-12-04 14:40:18 +0200

    locale

--- metrics.py
+++ metrics.py
@@ -13,7 +13,7 @@ def metricUserFlow():
 
 @metrics.route("/api/metrics/<click_type>/device", methods=["GET"])
 @cache.cached(timeout=1000, query_string=True)
-def getTotalClicks(click_type: str):
+def getTotalClicksDevice(click_type: str):
     available_click_type = ["totalClicks", "totalConversions", "totalShares"]
     if click_type not in available_click_type:
         return "Invalid click type", 400
@@ -65,6 +65,67 @@ def getTotalClicks(click_type: str):
                                 order by parse({grouping} as datetime) asc
                                 """).fetchall()
         out[device] = list(map(lambda x: {"period": x[1], "value": x[0]}, res))
+    return jsonify(out)
+
+@metrics.route("/api/metrics/<click_type>/locale", methods=["GET"])
+@cache.cached(timeout=1000, query_string=True)
+def getTotalClicksLocale(click_type: str):
+    available_click_type = ["totalClicks", "totalConversions", "totalShares"]
+    if click_type not in available_click_type:
+        return "Invalid click type", 400
+    
+    event = None
+    if click_type == 'totalConversions':
+        event = 'conversion'
+    elif click_type == 'totalShares':
+        event = "share_experience"
+    else:
+        event = "total"
+
+    timeframe = request.args.get("timeframe").lower(
+    ) if request.args.get("timeframe") is not None else "day"
+    timeframe = timeframe if timeframe in ["day", "week", "month"] else "day"
+    days = 1 if timeframe == "day" else 7 if timeframe == "week" else 30
+    grouping = "format(clicked_date, 'hh tt')" if timeframe == "day"\
+        else "format(clicked_date, 'yyyy-MM-dd')" if timeframe == "week"\
+        else "format(clicked_date, 'yyyy-MM-dd')"
+
+    cnxn = getConnection()
+    cursor = cnxn.cursor()
+    
+    locales = cursor.execute(f"""SELECT 	TOP(?)
+												locale
+										FROM Persons
+										GROUP BY locale
+										ORDER BY COUNT(locale) DESC""", 6).fetchall()
+    out = {}
+    for locale in locales:
+        locale = locale[0]
+        if event == "total":
+            res = cursor.execute(f"""
+                                declare @latest datetime = (select max(clicked_date) from Events)
+                                select count(*),
+                                    {grouping}
+                                from Events
+                                inner join Persons
+                                on Persons.person_id = Events.person_id
+                                where clicked_date > dateadd(day, {-days}, @latest) and Persons.locale = '{locale}'
+                                group by {grouping}
+                                order by parse({grouping} as datetime) asc
+                                """).fetchall()
+        else:
+            res = cursor.execute(f"""
+                                declare @latest datetime = (select max(clicked_date) from Events)
+                                select count(*),
+                                    {grouping}
+                                from Events
+                                inner join Persons
+                                on Persons.person_id = Events.person_id
+                                where clicked_date > dateadd(day, {-days}, @latest) and Persons.locale = '{locale}' and Events.event_name = '{event}'
+                                group by {grouping}
+                                order by parse({grouping} as datetime) asc
+                                """).fetchall()
+        out[locale] = list(map(lambda x: {"period": x[1], "value": x[0]}, res))
     return jsonify(out)
 
 @metrics.route("/api/metrics/average/<metric>", methods=["GET"])
