6cb65bbadc6a497fbc05f1940cbb75f95f7b4c2e
denis <dprodan289@gmail.com>
2022-12-04 14:13:43 +0200

    total share and conversion endpoints

--- metrics.py
+++ metrics.py
@@ -11,7 +11,6 @@ metrics = Blueprint('metrics', __name__)
 def metricUserFlow():
     pass
 
-
 @metrics.route("/api/metrics/totalClicks", methods=["GET"])
 @cache.cached(timeout=1000, query_string=True)
 def getTotalClicks():
@@ -38,8 +37,60 @@ def getTotalClicks():
 
     return jsonify(list(map(lambda x: {"period": x[1], "value": x[0]}, res)))
 
+@metrics.route("/api/metrics/conversionClicks", methods=["GET"])
+@cache.cached(timeout=1000, query_string=True)
+def getTotalConversion():
+    timeframe = request.args.get("timeframe").lower(
+    ) if request.args.get("timeframe") is not None else "day"
+    timeframe = timeframe if timeframe in ["day", "week", "month"] else "day"
+    days = 1 if timeframe == "day" else 7 if timeframe == "week" else 30
+    grouping = "format(clicked_date, 'hh tt')" if timeframe == "day"\
+        else "format(clicked_date, 'yyyy-MM-dd')" if timeframe == "week"\
+        else "format(clicked_date, 'yyyy-MM-dd')"
+
+    cnxn = getConnection()
+    cursor = cnxn.cursor()
+
+    res = cursor.execute(f"""
+                        declare @latest datetime = (select max(clicked_date) from Events)
+                        select count(*),
+                            {grouping}
+                        from Events
+                        where clicked_date > dateadd(day, {-days}, @latest) and event_name = 'conversion'
+                        group by {grouping}
+                        order by parse({grouping} as datetime) asc
+                        """).fetchall()
+
+    return jsonify(list(map(lambda x: {"period": x[1], "value": x[0]}, res)))
+
+@metrics.route("/api/metrics/shareClicks", methods=["GET"])
+@cache.cached(timeout=1000, query_string=True)
+def getTotalShares():
+    timeframe = request.args.get("timeframe").lower(
+    ) if request.args.get("timeframe") is not None else "day"
+    timeframe = timeframe if timeframe in ["day", "week", "month"] else "day"
+    days = 1 if timeframe == "day" else 7 if timeframe == "week" else 30
+    grouping = "format(clicked_date, 'hh tt')" if timeframe == "day"\
+        else "format(clicked_date, 'yyyy-MM-dd')" if timeframe == "week"\
+        else "format(clicked_date, 'yyyy-MM-dd')"
+
+    cnxn = getConnection()
+    cursor = cnxn.cursor()
+
+    res = cursor.execute(f"""
+                        declare @latest datetime = (select max(clicked_date) from Events)
+                        select count(*),
+                            {grouping}
+                        from Events
+                        where clicked_date > dateadd(day, {-days}, @latest) and event_name = 'share_experience'
+                        group by {grouping}
+                        order by parse({grouping} as datetime) asc
+                        """).fetchall()
+
+    return jsonify(list(map(lambda x: {"period": x[1], "value": x[0]}, res)))
+
 @metrics.route("/api/metrics/average/<metric>", methods=["GET"])
-@cache.memoize(timeout=1000)
+@cache.cached(timeout=1000, query_string=True)
 def getAverageMetric(metric: str):
     valid_metrics = ["clicksToConvert",
                      "clicksToShare", "timeToConvert", "timeToShare"]
@@ -71,3 +122,4 @@ def getAverageMetric(metric: str):
                         """).fetchall()
 
     return jsonify(list(map(lambda x: {"period": x[1], "value": x[0]}, res)))
+
